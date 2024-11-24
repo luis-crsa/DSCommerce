9e539b14f3515133b452e51a7835e01a17b721b1
Luis <luiscrsarmento@gmail.com>
2024-11-24 13:30:32 -0300

    GET Products paged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscommerce.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +21,15 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping(value = "/{id}")
-    public ProductDTO teste(@PathVariable Long id){
+    public ProductDTO findById(@PathVariable Long id){
         ProductDTO dto = productService.findById(id);
         return dto;
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<ProductDTO> list = productService.findAll(pageable);
+        return list;
     }
 
 }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dscommerce.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,4 +26,12 @@ public class ProductService {
         ProductDTO dto = new ProductDTO(product);
         return dto;
     }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = productRepository.findAll(pageable);
+        Page<ProductDTO> list = result.map(x -> new ProductDTO(x));
+        return list;
+    }
+
 }
